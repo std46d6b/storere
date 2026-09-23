fba0000fee605c72a90c8b59ce015395d83e3f21
std46d6b <174806133+std46d6b@users.noreply.github.com>
2026-09-23 00:03:20 +0000

    fix: style authenticated application screens

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,5 +4,6 @@ import { Provider } from 'react-redux'
 import { App } from './App'
 import { store } from './app/store'
 import './styles.css'
+import './auth.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><Provider store={store}><App /></Provider></StrictMode>)
